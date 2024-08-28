cceb19f27450d9c5bd17effdd2033d0d000fe10f
Tamaki Hosonuma <iidnelknbk@gmail.com>
2024-08-28 14:15:28 +0900

    gemlockの修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link sake_wood.jpg
 //= link star-half.png
 //= link star-off.png
 //= link star-on.png
